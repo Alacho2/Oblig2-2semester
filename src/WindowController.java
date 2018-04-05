1f07c68ee04c87eb2c588e77491fa598c0614b6b
Håvard <havard@Havards-MacBook-Pro.local>
2018-04-05 16:13:59 +0200

    First commit

---
+++ src/WindowController.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class WindowController extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+}
